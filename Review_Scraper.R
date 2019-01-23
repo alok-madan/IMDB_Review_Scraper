36bf97e895ed8d8570c42b321ad34bee34f66d7a
alok-madan <46357791+alok-madan@users.noreply.github.com>
2019-01-22 22:38:44 -0500

    Update Review_Scraper.R

--- Review_Scraper.R
+++ Review_Scraper.R
@@ -13,59 +13,20 @@ library(stringr)
 ID <- #######
 
 
-Date <- lapply(paste0('http://www.imdb.com/title/tt', ID, '/reviews?filter=prolific', 1:10),
-                function(url){
-                  url %>% read_html() %>% 
-                    html_nodes(".review-date") %>% 
-                    html_text() %>%
-                    gsub('[\r\n\t]', '', .)
-                  
-                })
 
-Stars <- lapply(paste0('http://www.imdb.com/title/tt', ID, '/reviews?filter=prolific', 1:10),
-                function(url){
-                  url %>% read_html() %>% 
-                    html_nodes(".rating-other-user-rating") %>% 
-                    html_text() %>%
-                    gsub('[\r\n\t]', '', .)
-                  
-                })
+IMDB.All <- lapply(paste0('http://www.imdb.com/title/tt', ID, '/reviews?filter=prolific', 1:10),
+                    function(url){
+                      url %>% read_html() %>% 
+                        html_nodes(".review-date,.rating-other-user-rating,.title,.show-more__control") %>% 
+                        html_text() %>%
+                        gsub('[\r\n\t]', '', .)
+                      
+                    })
+IMDB_Array <- unlist(IMDB.All)
 
-Title <- lapply(paste0('http://www.imdb.com/title/tt', ID, '/reviews?filter=prolific', 1:10),
-                function(url){
-                  url %>% read_html() %>% 
-                    html_nodes(".title") %>% 
-                    html_text() %>%
-                    gsub('[\r\n\t]', '', .)
-                  
-                })
-
-
-
-Review <- lapply(paste0('http://www.imdb.com/title/tt', ID, '/reviews?filter=prolific', 1:10),
-                  function(url){
-                    url %>% read_html() %>% 
-                      html_nodes(".show-more__control") %>% 
-                      html_text() %>%
-                      gsub('[\r\n\t]', '', .)
-                    
-                  })
-
-#Convert to Array
-
-Date <- unlist(Date)
-Title <- unlist(Title)
-Stars <- unlist(Stars)
-Review <- unlist(Review)
-
-merge <- cbind(Date,Title,Stars,Review)
-
-
-#Covert to Data Frame
-Final_Table <- data.frame(merge,stringsAsFactors = FALSE)
-
-#View Table
-View(Final_Table)
 
 #Export as CSV
-write.csv(Final_Table,file='IMDB_Reviews.csv')
+write.csv(IMDB_Array,file='IMDB_Reviews.csv')
+
+
+
